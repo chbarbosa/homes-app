0884ef375ce0f2d4b9c4d241e8de4518cd1437b6
Carlos Henrique Freitas Barbosa <carloshenriquefb@gmail.com>
2025-03-17 16:31:54 -0400

    applications listing fixed

--- src/app/applications/applications.component.ts
+++ src/app/applications/applications.component.ts
@@ -1,16 +1,53 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Application } from '../application';
+import { HousingService } from '../housing.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-applications',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   template: `
-    <p>
-      applications works!
-    </p>
+    <table>
+      <thead>
+        <tr>
+          <th>Location ID</th>
+          <th>First Name</th>
+          <th>Last Name</th>
+          <th>Actions</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr *ngFor="let application of applications">
+          <td>{{ application.housingLocationId }}</td>
+          <td>{{ application.firstName }}</td>
+          <td>{{ application.lastName }}</td>
+          <td>
+            <button (click)="onEdit(application)">Edit</button>
+            <button (click)="onDelete(application)">Delete</button>
+          </td>
+        </tr>
+      </tbody>
+    </table>
   `,
   styleUrl: './applications.component.css'
 })
 export class ApplicationsComponent {
+  applications : Application[] = [];
+  housingService: HousingService = inject(HousingService);
 
+  ngOnInit() {
+    this.housingService.listApplications().then(data => { 
+      console.log(data);
+      this.applications = data;
+    });
+  }
+
+  onEdit(application: any) {
+    // Implement edit functionality
+  }
+
+  onDelete(application: any) {
+    // Implement delete functionality
+  }
 }
