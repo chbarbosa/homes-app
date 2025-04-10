3c7023be4c70598e62502ee2ff99ce6d88969f31
Carlos Henrique Freitas Barbosa <carloshenriquefb@gmail.com>
2025-04-10 16:39:01 -0400

    confirmation finished

--- src/app/applicationsconfirmation/applicationsconfirmation.component.ts
+++ src/app/applicationsconfirmation/applicationsconfirmation.component.ts
@@ -1,16 +1,36 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-applicationsconfirmation',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   template: `
-    <p>
-      applicationsconfirmation works!
-    </p>
+    <article class="confirmation">
+      <h2>Thank you for your application!</h2>
+      <section class="details" *ngIf="application">
+        <p>Dear {{application.firstName}} {{application.lastName}},</p>
+        <p>Your application has been received and we will contact you at <strong>{{application.email}}</strong> regarding next steps.</p>
+      </section>
+    </article>
   `,
-  styleUrl: './applicationsconfirmation.component.css'
+  styleUrls: ['./applicationsconfirmation.component.css']
 })
 export class ApplicationsconfirmationComponent {
+  application: any;
+  houseName: string = '';
 
+  constructor(private router: Router) {
+    const navigation = this.router.getCurrentNavigation();
+    const state = navigation?.extras.state as { application: any };
+    
+    if (state && state.application) {
+      this.application = state.application;
+      this.houseName = state.application.houseName || 'Not specified';
+    } else {
+      // Redirect to home if no application data
+      this.router.navigate(['/']);
+    }
+  }
 }
